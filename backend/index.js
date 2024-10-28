2da381d191b960cd22c9cd0dfb3cb7dba5a3c03d
Sliepix <victor.ande@icloud.com>
2024-10-28 23:46:21 +0100

    react innit

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,25 @@
-const express = require('express')
+const dotenv = require('dotenv'),
+  { Client } = require('pg')
+
+dotenv.config()
+
+const client = new Client({
+  connectionString: process.env.PGURI
+})
+
+client.connect()
+
+const express = require('express'),
+  path = require('path')
 
 const app = express(),
   port = process.env.PORT || 3000
+  
+  app.get('/', (_request, response) => {
+    response.send({ hello: 'World' })
+  })
 
-app.get('/', (_request, response) => {
-  response.send({ hello: 'World' })
-})
+app.use(express.static(path.join(path.resolve(), 'dist')))
 
 app.listen(port, () => {
   console.log(`Redo på http://localhost:${port}/`)
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,11 +1,19 @@
-import type { Config } from 'tailwindcss';
+import type { Config } from "tailwindcss";
 
-export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
   },
-
-  plugins: []
-} satisfies Config;
+  plugins: [],
+};
+export default config;
